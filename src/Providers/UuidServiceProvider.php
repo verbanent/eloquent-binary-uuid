e315877f7e6a6d2885a035a3cdd30310bb7a6d66
Radoslaw Ziemniewicz <admin@verbanent.com>
2019-03-12 16:24:55 +0000

    Added MySqlGrammar and UuidServiceProvider to have binary type of uuid columns

---
+++ src/Providers/UuidServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Verbanent\Uuid\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Verbanent\Uuid\Grammars\MySqlGrammar;
+
+/**
+ * Service provides grammar to change UUID default column type to binary.
+ */
+class UuidServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        $grammar = new MySqlGrammar();
+        $connection = app('db')->connection();
+        $connection->setSchemaGrammar($grammar);
+    }
+}
