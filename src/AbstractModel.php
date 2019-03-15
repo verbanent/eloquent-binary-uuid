c1184408c88ce9d24537f646c42a52d73acaaa52
Radoslaw Ziemniewicz <admin@verbanent.com>
2019-03-15 22:33:33 +0000

    Added abstract model which can be used as parent for all model class using UUID binary primary keys

---
+++ src/AbstractModel.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Verbanent\Uuid;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class can be used as parent for all model class using UUID binary primary keys.
+ */
+class AbstractModel extends Model
+{
+    /**
+     * Disable incrementing of ID column.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Type for primary key.
+     *
+     * @var string
+     */
+    protected $keyType = 'uuid';
+
+    /**
+     * Column name for primary key.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+}
