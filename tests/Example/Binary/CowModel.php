1a03677925157b379c32649645bb871f5b00997e
Radoslaw Ziemniewicz <admin@verbanent.com>
2019-03-22 16:41:57 +0000

    Added test example classes

---
+++ tests/Example/Binary/CowModel.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Verbanent\Uuid\Test\Example\Binary;
+
+use Verbanent\Uuid\Test\Example\AbstractExampleModel;
+
+/**
+ * Cow model.
+ */
+class CowModel extends AbstractExampleModel
+{
+
+}
---
+++ tests/Example/Binary/DogModel.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Verbanent\Uuid\Test\Example\Binary;
+
+use Verbanent\Uuid\Test\Example\AbstractExampleModel;
+
+/**
+ * Dog model.
+ */
+class DogModel extends AbstractExampleModel
+{
+
+}
